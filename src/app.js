97964114609464f506859cc9b11d42009caaf04e
sac1010 <sachingirish101@gmail.com>
2023-12-19 17:33:20 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 mongoose.connect(
